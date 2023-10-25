07837778f4106e4de6dc959b5b881393433b4527
Your Name <your-email@example.com>
2023-10-25 19:21:35 +0300

    hw03

--- src/4.ts
+++ src/4.ts
@@ -1,9 +1,60 @@
+
+
+class Key{
+    private signature:number= Math.random();
+    getSignature(): number{
+        return this.signature;
+    }
+}
+
+class Person {
+    private myKey: Key;
+
+    constructor(newSignature: Key) {
+        this.myKey = newSignature;
+    }
+
+    getKey(){
+        return this.myKey;
+    }}
+
+abstract class House {
+    protected isOpen: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+    constructor(key: Key){
+        this.isOpen=false;
+        this.key=key;
+    }
+    comeIn(person: Person){
+if(this.isOpen){
+    this.tenants.push(person);
+}}
+abstract OpenDoor(key: Key): void;
+    }
+
+
+
+
+    class MyHouse extends House {
+        OpenDoor(key: Key) {
+            if (key.getSignature() === this.key.getSignature()) {
+                this.isOpen = true;
+            }
+        }
+    }
+    
+    
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
 
